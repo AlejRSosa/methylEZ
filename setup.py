0f2142f648470f041dec74d9413a144ebbd03ae6
Alejandra Rodriguez-Sosa <99178046+AlejRSosa@users.noreply.github.com>
2025-02-07 17:55:21 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ setup(
     name="methylEZ",
     version="0.1",
     packages=find_packages(),
+    include_package_data=True,  # Ensure all files are included
     install_requires=[
         "pandas",
         "pyperclip",
     ],
     entry_points={
         "console_scripts": [
-            "methylEZ = main:main",  # Ensure this points to main.py
+            "methylEZ = methylEZ.main:main",  # Ensure correct package structure
         ],
     },
 )
